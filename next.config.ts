ed2c253debe38751ff91cd202b635f7e1e03fba1
yanuararysyahputra <yanuarary088@gmail.com>
2026-05-19 13:34:13 +0700

    fix dynamic1214\52

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  experimental: {
+    serverActions: {},
+  },
+
+  output: "standalone",
 };
 
 export default nextConfig;
